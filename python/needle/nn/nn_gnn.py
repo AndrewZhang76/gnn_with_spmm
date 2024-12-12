424a943719471c2a0807b01befdc8d36c50dcced
Yimei <yimeiwu0606@gmail.com>
2024-12-11 19:02:36 -0500

    update sparse gcn

--- python/needle/nn/nn_gnn.py
+++ python/needle/nn/nn_gnn.py
@@ -11,7 +11,8 @@ class GraphConvolution(Module):
         self.bias = Parameter(init.kaiming_uniform(out_features, 1, device=device, dtype=dtype).reshape((1, out_features))) if bias else None
 
     def forward(self, X: Tensor, A: Tensor) -> Tensor:
-        out = ops.matmul(A, X)
+        sparse_A = A.to_sparse()
+        out = ops.sparse_matmul(A, X)
         out = ops.matmul(out, self.weight)
         if self.bias is not None:
             out = ops.add(out, self.bias.broadcast_to(out.shape))
